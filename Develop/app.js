83e6c61770f2670ba0e2a3e25917e3bde5ae43c1
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-06-10 22:59:56 -0700

    adding style and comments

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,12 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+// empty arrays to push new members
 const teamMembers = [];
 const idArray = [];
- createManager();
+
+createManager();
+// prompts user in node to input info about manager
 function createManager() {
     console.log("please build your team");
     inquirer.prompt([
@@ -21,7 +23,6 @@ function createManager() {
             type: "input",
             message: "what is your managers name",
             name: "managerName",
-            //validate
         },
         {
             type: "input",
@@ -39,17 +40,22 @@ function createManager() {
             name: "managerNum",
         }
 
-    ]).then(function(response) {
+    ]).then(response => {
+        // grabs object name ids into their own response. and is stored into manager 
         //console.log(response.managerName)
-     const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerNum);
-     teamMembers.push(manager);
-     idArray.push(response.managerId);
-     createTeam();
-// console.log(teamMembers);
-// console.log(idArray)
-      });
-    //fs.writeFile(outputPath);
+        const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerNum);
+        // pushes teammember array to manager 
+        teamMembers.push(manager);
+        //pushes the managers id to idArray
+        idArray.push(response.managerId);
+        createTeam();
+        // console.log(teamMembers);
+        // console.log(idArray)
+    });
+
 }
+// prompts user in node to input info about engineer
+
 function addEngineer() {
     //console.log("please build your team");
     inquirer.prompt([
@@ -57,7 +63,7 @@ function addEngineer() {
             type: "input",
             message: "what is your Engineers name",
             name: "engineerName",
-            //validate
+
         },
         {
             type: "input",
@@ -75,26 +81,26 @@ function addEngineer() {
             name: "engineerGithub",
         }
 
-    ]).then(function(response) {
-        //console.log(response.managerName)
-     const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
-     teamMembers.push(engineer);
-     idArray.push(response.EngineerId);
-    createTeam();
-// console.log(teamMembers);
-// console.log(idArray)
-      });
+    ]).then(response => {
+        const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+        teamMembers.push(engineer);
+        idArray.push(response.EngineerId);
+        createTeam();
+        // console.log(teamMembers);
+        // console.log(idArray)
+    });
 
 }
 
 function addIntern() {
-    //console.log("please build your team");
+// prompts user in node to input info about intern
+
     inquirer.prompt([
         {
             type: "input",
             message: "what is your Interns name",
             name: "internName",
-            //validate
+
         },
         {
             type: "input",
@@ -112,50 +118,48 @@ function addIntern() {
             name: "internSchool",
         }
 
-    ]).then(function(response) {
-        //console.log(response.managerName)
-     const intern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
-     teamMembers.push(intern);
-     idArray.push(response.InternId);
-    createTeam();
-// console.log(teamMembers);
-// console.log(idArray)
-      });
+    ]).then(response => {
+
+        const intern = new Intern(response.internName, response.internId, response.internEmail, response.internSchool);
+        teamMembers.push(intern);
+        idArray.push(response.InternId);
+        createTeam();
+    });
 
 }
-
-function createTeam(){
+// prompts user wheter or not they would like to add a new teammember or to not add anymore
+function createTeam() {
     inquirer.prompt([
         {
             type: "list",
-            name:"memberChoice",
+            name: "memberChoice",
             message: "which type of team member would you like to add?",
-            choices:[
+            choices: [
                 "Engineer",
                 "Intern",
                 "I dont want to add any more team members"
             ]
         }
-    ]).then(userChoice =>{
-        switch(userChoice.memberChoice){
+    ]).then(userChoice => {
+        switch (userChoice.memberChoice) {
             case "Engineer":
                 addEngineer();
                 break;
-                case "Intern":
-                    addIntern();
-                    break;
-                    default:
-                        buildTeam();
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                buildTeam();
         }
     })
 }
 
 
-function buildTeam(){
-    console.log("bff")
+function buildTeam() {
+    // console.log("unison")
     //create the output path if it doesnt exist
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
-      }
-fs.writeFileSync(outputPath,render(teamMembers),"utf-8")
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
 }
